3451f30887c4583c130bcb553524b0173de4dcc1
mrbrand <peter.brand79@gmail.com>
2016-09-19 11:15:48 +0200

    Signed-off-by: mrbrand <peter.brand79@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# godfather
+# WiseGuy
+Task manager with HTML, JS and local storage. 
+No installation required, all data is saved locally. You can add WiseGuy as an app to your smartphone by opening the link below and clicking 'Add to Home screen' in your smartphone browser.
+
+# Latest version 
+Klick this link to open the latest version of WiseGuy:
+https://rawgit.com/Mrbrand/WiseGuy/master/index.html
+
+#Screenshots
+![alt tag](https://raw.github.com/Mrbrand/WiseGuy/master/img/screenshot.png)
+
+##Why make another task app?
+Well, I have tested a whole lot of task manager apps throughout the years. 
+But none of them completely satisfies my requirements. 
+* Tree structured tasks
+* Task prio
+* Task types
+* Task size
+* Preview subtasks
+* Filter by prio
+* Filter by type
---
+++ array_query.js
@@ -0,0 +1,37 @@
+//hello master
+
+Array.prototype.query = function (field, operator, value) { 
+    
+    if (operator=="contains"){
+        value = value.toLowerCase();
+		return this.filter(function (item){
+		 	return item[field].toLowerCase().indexOf(value) !== -1; 	
+		});
+	}
+	
+	else if(operator == "==") {
+		return this.filter(function (item){
+			 	return item[field] == value;
+		});
+	}
+    
+    else if(operator == "!=") {
+    	return this.filter(function (item){
+			 	return item[field] != value;
+		});
+	}
+    
+    else if(operator == ">") {
+        return this.filter(function (item){
+			 	return item[field] > value;
+		});
+	}
+    
+    
+    else if(operator == "<") {
+        return this.filter(function (item){
+    		 	return item[field] < value;
+		});
+	}
+    
+}
